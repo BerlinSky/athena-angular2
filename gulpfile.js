863c706b572ee3ccadb9073cbf38726c7826e786
Berlin Sky <pedroyoung8989@gmail.com>
2016-07-16 10:28:26 -0700

    Set sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,44 @@
 var gulp = require('gulp');
-
 var assetsDev = 'assets/';
 var assetsProd = 'src/';
-
+var appSrc = 'src/';
 var appDev = 'dev/';
 var dist = 'dist/';
 
-/* CSS */
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 var cssClean = require('gulp-clean-css');
 
-/* JS & TS */
 var typescript = require('gulp-typescript');
-
 var tsProject = typescript.createProject('tsconfig.json');
 
 gulp.task('build-css', function () {
-    return gulp.src(assetsDev + 'scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({}).on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(autoprefixer({
-            browsers: ['last 3 versions'],
-            cascade: false
-        }))
-        .pipe(cssClean({compatibility: 'ie8'}))
-        .pipe(gulp.dest(assetsProd + 'css/'));
+  return gulp.src(appSrc + 'sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({
+        browsers: ['last 3 versions'],
+        cascade: false
+    }))
+    .pipe(cssClean({compatibility: 'ie8'}))
+    .pipe(gulp.dest(dist + 'styles/css/'));
 });
 
 gulp.task('build-ts', function () {
-    return gulp.src(appDev + '**/*.ts')
-        .pipe(sourcemaps.init())
-        .pipe(typescript(tsProject))
-        .pipe(sourcemaps.write())
-        //.pipe(jsuglify())
-        .pipe(gulp.dest(dist));
+  return gulp.src(appDev + '**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(typescript(tsProject))
+    .pipe(sourcemaps.write())
+    //.pipe(jsuglify())
+    .pipe(gulp.dest(dist));
 });
 
 gulp.task('bundle-ts', ['build-ts'], function() {
   var path = require("path");
   var Builder = require('systemjs-builder');
 
-  // optional constructor options
-  // sets the baseURL and loads the configuration file
   var builder = new Builder('', 'systemjs.config.js');
 
   builder
