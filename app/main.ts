4b7a27fa50072a888441d00091fc841886b0683a
Berlin Sky <pedroyoung8989@gmail.com>
2016-12-01 18:54:29 -0800

    Set up the base page

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>The Book Reading World</h1>'
+  template: `<h1>{{title}}</h1>
+    <h2>{{book.name}}</h2>
+    <p>{{book.description}}</p>
+    <p>{{book.inStock}} copies in stock</p>`
 })
 class AppComponent {
-
+  title = "The Book Reading Life";
+  book = {
+    "id": 1,
+    "name": "Why save the bankers?",
+    "description": "The articles from a French economist",
+    "inStock": 15
+  };
 }
 
 @NgModule({
