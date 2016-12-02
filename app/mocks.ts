38e95235d33a5b048864c470b48f2198c5d7ca55
Berlin Sky <pedroyoung8989@gmail.com>
2016-12-02 14:44:53 -0800

    Mocks and models

---
+++ app/mocks.ts
@@ -0,0 +1,33 @@
+import { Book } from './book';
+
+export const BOOKS: Book[] = [{
+    "id": 1,
+    "name": "Why save the bankers?",
+    "description": "The articles from a French economist",
+    "price": 13.50,
+    "inStock": 15
+  },
+  {
+    "id": 2,
+    "name": "Middlemarch",
+    "description": "A complex tale of idealism, disillusion, profligacy, loyalty and frustrated love.",
+    "price": 35.50,
+    "inStock": 20
+  },
+  {
+    "id": 3,
+    "name": "The Idiot",
+    "description": "A portrait of a man of pure innocence",
+    "price": 29.35,
+
+    "inStock": 0
+  },
+  {
+    "id": 4,
+    "name": "Jane Eyre",
+    "description": "A novel of intense power and intrigue",
+    "price": 20.95,
+
+    "inStock": 27
+  }
+];
