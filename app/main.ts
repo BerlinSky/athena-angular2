84dca256f1ac35773b33d42c3261869641bf2009
Berlin Sky <pedroyoung8989@gmail.com>
2016-12-01 18:43:53 -0800

    Set up the base page

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,25 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>The Book Reading World</h1>'
+})
+class AppComponent {
+
+}
+
+@NgModule({
+  declarations: [ AppComponent ],
+  imports: [ BrowserModule ],
+  bootstrap: [ AppComponent ]
+})
+class AppModule {
+
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
