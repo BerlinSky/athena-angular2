62d946e4db4205d093b3cda26d253fe88fd3a9b4
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-03 21:19:51 -0800

    Clean up

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
 
+export class Artist {
+  id: number;
+  name: string;
+  webSite: string;
+}
+
 @Component({
   selector: 'artist-app',
   template: `
-      <h1>Welcome {{ title }}</h1>
-    `,
+    <h1>Welcome to {{ title }}</h1>
+    <h3>Site Link: {{ artist.webSite }}</h3>
+    <div>
+      <span>Artist: </span>{{ artist.name }}
+    </div>
+    <div>
+      <div>
+        <label>Web Site: </label>
+      </div>
+      <div>
+        <input [(ngModel)]="artist.webSite" placeholder="webSite">
+      </div>
+    </div>
+  `,
 })
-export class AppComponent  { title = 'New Artist Site'; }
+export class AppComponent  {
+  title = 'New Artist Site';
+
+  artist: Artist = {
+    id: 101,
+    name: "Tokyo Night",
+    webSite: "www.tokyonight.com"
+  }
+}
