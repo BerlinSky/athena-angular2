fd12f946d2c22b3fac74565a6705b88bb1ddb512
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-07 19:56:19 -0800

    Update

--- app/portfolio.component.ts
+++ app/portfolio.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Artist } from './artist';
+import { ArtistService } from './artist.service';
 
 @Component({
   moduleId: module.id,
   selector: 'artist-portfolio',
   templateUrl: 'portfolio.component.html'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
   title = "The detail is in the html template"
+
+  artists: Artist[] = [];
+
+  constructor(private artistService: ArtistService) { }
+
+  ngOnInit(): void {
+    this.artistService.getArtists()
+      .then(artists => this.artists = artists);
+  }
 }
