450ab22f77ee78fbbaf56bbda9fedb24abd23e16
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-11 10:37:35 -0800

    Update

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,21 +4,46 @@ var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'cheap-module-eval-source-map',
-
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  target: 'web',
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://localhost:8080/',
+  //   publicPath: 'http://localhost:8080/',
+    ppublicPath: '/',
+    // filename: 'bundle.js'
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
-
   plugins: [
     new ExtractTextPlugin('[name].css')
   ],
-
   devServer: {
     historyApiFallback: true,
     stats: 'minimal'
   }
 });
+
+
+// export default {
+//   debug: true,
+//   devtool: 'inline-source-map',
+//   noInfo: false,
+//   entry: [
+//     path.resolve(__dirname, 'src/index')
+//   ],
+//   target: 'web',
+//   output: {
+//     path: path.resolve(__dirname, 'src'),
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   plugins: [],
+//   module: {
+//     loaders: [
+//       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+//       { test: /\.css$/, loaders: ['style', 'css'] }
+//     ]
+//   }
+// }
