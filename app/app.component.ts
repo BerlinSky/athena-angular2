f276217b797d1e5e767214edf015718e36ee3795
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-10 20:12:01 -0800

    Update

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'artist-app',
-  template: `
-      <h1>Welcome {{ title }}</h1>
-    `,
-})
-export class AppComponent  { title = 'New Artist Site'; }
