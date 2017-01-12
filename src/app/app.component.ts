3822bf97e1b0fbf36a74b3fa34e2102560aae389
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-11 18:08:16 -0800

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,33 @@ export class Artist {
   webSite: string;
 }
 
+const ARTISTS: Artist[] = [
+  { id: 101, name: 'Carrie Underwood', webSite: "www.artist-site.com" },
+  { id: 102, name: 'U2', webSite: "www.artist-site.com" },
+  { id: 103, name: 'Taylor Swift', webSite: "www.artist-site.com" },
+  { id: 104, name: 'Beyonce', webSite: "www.artist-site.com" },
+  { id: 105, name: 'Adele', webSite: "www.artist-site.com" },
+  { id: 106, name: 'Justin Bieber', webSite: "www.artist-site.com" },
+  { id: 107, name: 'Jennifer Lopez', webSite: "www.artist-site.com" },
+  { id: 108, name: 'Lady Gaga', webSite: "www.artist-site.com" },
+  { id: 109, name: 'Katy Perry', webSite: "www.artist-site.com" },
+  { id: 110, name: 'Madonna', webSite: "www.artist-site.com" }
+];
+
 @Component({
   selector: 'artist-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'New Artist Site';
+  title = 'Artist Sites';
 
-  artist: Artist = {
-    id: 101,
-    name: "Tokyo Night",
-    webSite: "www.tokyonight.com"
+  artists = ARTISTS;
+  artistSelected: Artist;
+
+  selectArtist(artist: Artist): void {
+    console.log(artist);
+
+    this.artistSelected = artist;
   }
 }
-
-
-
-// @Component({
-//   selector: 'artist-app',
-//   template: `
-//     <h1>Welcome to {{ title }}</h1>
-//     <h3>Site Link: {{ artist.webSite }}</h3>
-//     <div class="artist">
-//       Artist: <span>{{ artist.name }}</span>
-//     </div>
-//     <div class="artist">
-//         Web Site:
-//         <input [(ngModel)]="artist.webSite" placeholder="webSite">
-//     </div>
-//   `,
-// })
-// export class AppComponent  {
-//   title = 'New Artist Site';
-
-// }
