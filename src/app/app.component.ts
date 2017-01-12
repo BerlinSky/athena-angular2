9f4ecff8be55d355d42837a5803334574c822106
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-11 16:29:43 -0800

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.css';
 
+export class Artist {
+  id: number;
+  name: string;
+  webSite: string;
+}
+
 @Component({
   selector: 'artist-app',
   templateUrl: './app.component.html',
@@ -8,4 +14,31 @@ import '../../public/css/styles.css';
 })
 export class AppComponent {
   title = 'New Artist Site';
+
+  artist: Artist = {
+    id: 101,
+    name: "Tokyo Night",
+    webSite: "www.tokyonight.com"
+  }
 }
+
+
+
+// @Component({
+//   selector: 'artist-app',
+//   template: `
+//     <h1>Welcome to {{ title }}</h1>
+//     <h3>Site Link: {{ artist.webSite }}</h3>
+//     <div class="artist">
+//       Artist: <span>{{ artist.name }}</span>
+//     </div>
+//     <div class="artist">
+//         Web Site:
+//         <input [(ngModel)]="artist.webSite" placeholder="webSite">
+//     </div>
+//   `,
+// })
+// export class AppComponent  {
+//   title = 'New Artist Site';
+
+// }
