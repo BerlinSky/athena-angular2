1c8e31070f0f2f733689c9a4cdd94aabaf2ac49c
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-11 18:52:03 -0800

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+
+import { Artist } from '../artist/artist';
+
 import '../../public/css/styles.css';
 
-export class Artist {
-  id: number;
-  name: string;
-  webSite: string;
-}
+// export class Artist {
+//   id: number;
+//   name: string;
+//   webSite: string;
+// }
 
 const ARTISTS: Artist[] = [
   { id: 101, name: 'Carrie Underwood', webSite: "www.artist-site.com" },
