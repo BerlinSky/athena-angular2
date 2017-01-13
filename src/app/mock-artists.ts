7a0e8a688028b219fb83d6c2dce8b49fb139f8bb
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-13 07:44:10 -0800

    Update

---
+++ src/app/mock-artists.ts
@@ -0,0 +1,14 @@
+import { Artist } from '../artist/artist';
+
+export const ARTISTS: Artist[] = [
+  { id: 101, name: 'Carrie Underwood', webSite: "www.artist-site.com" },
+  { id: 102, name: 'U2', webSite: "www.artist-site.com" },
+  { id: 103, name: 'Taylor Swift', webSite: "www.artist-site.com" },
+  { id: 104, name: 'Beyonce', webSite: "www.artist-site.com" },
+  { id: 105, name: 'Adele', webSite: "www.artist-site.com" },
+  { id: 106, name: 'Justin Bieber', webSite: "www.artist-site.com" },
+  { id: 107, name: 'Jennifer Lopez', webSite: "www.artist-site.com" },
+  { id: 108, name: 'Lady Gaga', webSite: "www.artist-site.com" },
+  { id: 109, name: 'Katy Perry', webSite: "www.artist-site.com" },
+  { id: 110, name: 'Madonna', webSite: "www.artist-site.com" }
+];
