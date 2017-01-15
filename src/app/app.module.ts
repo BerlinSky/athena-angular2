a979f83c966d31a10af2fcd2b2aa61d1b9830f7b
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-14 20:27:38 -0800

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ArtistComponent } from '../artist/artist.component'
 import { ArtistDetailComponent } from '../artist-detail/artist-detail.component'
 
 @NgModule({
@@ -12,6 +13,7 @@ import { ArtistDetailComponent } from '../artist-detail/artist-detail.component'
   ],
   declarations: [
     AppComponent,
+    ArtistComponent,
     ArtistDetailComponent
   ],
   bootstrap: [ AppComponent ]
